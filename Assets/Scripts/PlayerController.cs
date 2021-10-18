be3846e183ad6b9fed19ae25afd571c99d775132
saifxss <60525693+saifxss@users.noreply.github.com>
2021-10-18 21:44:45 +0100

    test camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,13 +10,24 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private Vector2 walkInput;
     private float jumpInput;
+    private Vector3 velocity;
     [SerializeField] float Speed;
     [SerializeField] float jumpforce;
+    private float angle;
+    public float turnspeed=8f;
+    private float smoothinputvelocity;
+    private float smoothmovetime;
+    private float smoothinputMagnitude;
+    private bool grounded;
+    public Animator myanimator;
+
+
 
     void Start()
     {
         mycc = GetComponent<CharacterController>();
         rb = GetComponent<Rigidbody>();
+        myanimator = GetComponent<Animator>();
     }
     public void OnWalk(InputAction.CallbackContext context)
     {
@@ -33,24 +44,39 @@ public class PlayerController : MonoBehaviour
         if (walkInput.x != 0 || walkInput.y != 0)
         {
             move();
-           // myanimator.SetBool("ismoving", true);
+            myanimator.SetBool("ismoving", true);
             Debug.Log("moved");
         }
 
         else
         {
-           // myanimator.SetBool("ismoving", false);
+            myanimator.SetBool("ismoving", false);
 
         }
-        if (jumpInput!=0)
+        if (jumpInput!=0&&grounded)
         {
             rb.velocity += jumpforce * Vector3.up;
+            myanimator.SetTrigger("isJumping");
             Debug.Log("jumped");
         }
+
+        rb.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+            rb.MovePosition(rb.position + velocity * Time.deltaTime);
+
     }
     public void move()
     {
-        float y = Mathf.Atan2(walkInput.x, walkInput.y) * Mathf.Rad2Deg;
+        Vector3 inputDirection = Vector3.zero;
+
+            inputDirection = new Vector3(walkInput.x, 0, walkInput.y).normalized;
+
+
+        float inputMagnitude = inputDirection.magnitude;
+        smoothinputMagnitude = Mathf.SmoothDamp(smoothinputMagnitude, inputMagnitude, ref smoothinputvelocity, smoothmovetime);
+
+        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnspeed * inputMagnitude);
+        /*float y = Mathf.Atan2(walkInput.x, walkInput.y) * Mathf.Rad2Deg;
         Vector3 Direction = new Vector3(walkInput.x, 0, walkInput.y);
         Direction.Normalize();
 
@@ -61,7 +87,25 @@ public class PlayerController : MonoBehaviour
             transform.forward = Direction;
         }
         transform.rotation = rot;
-
+        */
         rb.velocity = new Vector3(walkInput.x * Speed, rb.velocity.y, walkInput.y * Speed);
+ 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            Debug.Log("is grounded");
+            myanimator.SetBool("JumpEnd",true);
+            grounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            myanimator.SetBool("JumpEnd", false);
+            grounded = false;
+        }
     }
 }
