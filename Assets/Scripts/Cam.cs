b0bdda97f67776250e43653a8ab518abd246a2fd
saifxss <60525693+saifxss@users.noreply.github.com>
2021-10-21 22:52:27 +0100

    test

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -1,25 +1,75 @@
-using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+[RequireComponent(typeof(InputHandler))]
 public class Cam : MonoBehaviour
 {
+    private InputHandler _input;
 
-    public GameObject player;        //Public variable to store a reference to the player game object
+    [SerializeField]
+    private bool RotateTowardMouse;
 
+    [SerializeField]
+    private float MovementSpeed;
+    [SerializeField]
+    private float RotationSpeed;
 
-    private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+    [SerializeField]
+    private Camera Camera;
 
-    // Use this for initialization
-    void Start()
+    private void Awake()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+        _input = GetComponent<InputHandler>();
     }
 
-    // LateUpdate is called after Update each frame
-    void LateUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+
+        var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
+        var movementVector = MoveTowardTarget(targetVector);
+
+        if (!RotateTowardMouse)
+        {
+            RotateTowardMovementVector(movementVector);
+        }
+        if (RotateTowardMouse)
+        {
+            RotateFromMouseVector();
+        }
+
+    }
+
+    private void RotateFromMouseVector()
+    {
+        Ray ray = Camera.ScreenPointToRay(_input.MousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
+        {
+            var target = hitInfo.point;
+            target.y = transform.position.y;
+            transform.LookAt(target);
+        }
+    }
+
+    private Vector3 MoveTowardTarget(Vector3 targetVector)
+    {
+        var speed = MovementSpeed * Time.deltaTime;
+        // transform.Translate(targetVector * (MovementSpeed * Time.deltaTime)); Demonstrate why this doesn't work
+        //transform.Translate(targetVector * (MovementSpeed * Time.deltaTime), Camera.gameObject.transform);
+
+        targetVector = Quaternion.Euler(0, Camera.gameObject.transform.rotation.eulerAngles.y, 0) * targetVector;
+        var targetPosition = transform.position + targetVector * speed;
+        transform.position = targetPosition;
+        return targetVector;
+    }
+
+    private void RotateTowardMovementVector(Vector3 movementDirection)
+    {
+        if (movementDirection.magnitude == 0) { return; }
+        var rotation = Quaternion.LookRotation(movementDirection);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, RotationSpeed);
     }
 }
