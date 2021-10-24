32c7e95d2df50b6f6e74ce82a2ac09906db3092a
saifxss <60525693+saifxss@users.noreply.github.com>
2021-10-24 23:45:54 +0100

    Update 0.3

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +8,70 @@ public class CharacterSelection : MonoBehaviour
 {
     [SerializeField]
     private InputAction RL;
+    [SerializeField]
+    private InputAction _ready;
+    private float _RL;
+    private int _currentChild;
+    public static int n = 0;
+    bool isReady = false;
+
     private void Awake()
     {
-        //RL.performed+=
+        RL.performed += ONRLPreformed;
+        RL.canceled += ONRLPreformed;
+        _ready.performed += Ready;
+    }
+    private void OnEnable()
+    {
+        RL.Enable();
+        _ready.Enable();
+    }
+    private void OnDisable()
+    {
+        RL.Disable();
+        _ready.Disable();
+    }
+    private void ONRLPreformed(InputAction.CallbackContext context)
+    {
+        _RL = RL.ReadValue<float>();
+        if (_RL != 0)
+        {
+            if (_currentChild == 0)
+                _currentChild = 1;
+            else
+                _currentChild = 0;
+            SwitchCharacter(_currentChild);
+        }
+    }
+    void Ready(InputAction.CallbackContext context)
+    {
+        if (isReady) 
+            return;
+        Debug.Log("ready");
+        int currentPlayer = (transform.name == "P1" ? 0 : 1);
+        n++;
+        isReady = true;
+        PlayerPrefs.SetInt("CharacterNumber" + currentPlayer, _currentChild); PlayerPrefs.Save();
 
+        if (n == 2)
+            AllReady();
+    }
+    void AllReady()
+    {
+        Debug.Log("All Ready");
+    }
+
+    private void SwitchCharacter(int _CurrentChild)
+    {
+        if (_CurrentChild == 0)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            transform.GetChild(1).gameObject.SetActive(false);
+        }
+        else
+        {
+            transform.GetChild(1).gameObject.SetActive(true);
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
     }
 }
