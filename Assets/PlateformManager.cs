f554c6ee2606f482ee3f9e65afebc322ce8d4168
ghalba <cherifaghalba@gmail.com>
2021-10-31 20:55:26 +0100

    fruit fall

---
+++ Assets/PlateformManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateformManager : MonoBehaviour
+{
+    public GameObject[] plateforms;
+    public int img;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 12; i++)
+        {
+           //img = Random.range
+
+        }   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
