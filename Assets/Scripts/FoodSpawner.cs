c6d5f1d44f611507509c5e4641763ac30d7be167
saifxss <60525693+saifxss@users.noreply.github.com>
2021-11-17 15:31:56 +0100

    15.31

---
+++ Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour
+{
+    public GameObject _finishPoint;
+    List<Transform> transforms;
+    List<GameObject> Food;
+
+}
