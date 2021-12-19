e9ed92a4538de9f0a0835ddba5e5a3486b412d71
saifxss <60525693+saifxss@users.noreply.github.com>
2021-12-19 20:41:57 +0100

    blur

--- Assets/Animals/AnimalSpawner.cs
+++ Assets/Animals/AnimalSpawner.cs
@@ -19,36 +19,90 @@ public class AnimalSpawner : MonoBehaviour
     int raccooncount = 0;
     int foxcount = 0;
     int tigercount = 0;
-    void Spawn1()
+    public bool _Spawn1;
+    public bool _Spawn2;
+
+    private void Start()
+    {
+        _Spawn1 = true;
+    }
+    private void Update()
     {
         // Stage 1
+        if (MaxSpawn < 10 && _Spawn1)
+        {
+            StartCoroutine(Timer1(1f));
+            
+        }
+        Debug.Log(MaxSpawn);
+        if (MaxSpawn == 10&& _Spawn1)
+        {
+            MaxSpawn = 0;
+            bearcount = 0;
+            wolfcount = 0;
+            deercount = 0;
+            raccooncount = 0;
+            foxcount = 0;
+            tigercount = 0;
+            _Spawn1 = false;
+            _Spawn2 = true;
+        }
+        // Q&A 1
+
+        // Stage 2
+        if (MaxSpawn < 15 && _Spawn2)
+        {
+            Debug.Log("Stage 2");
+            StartCoroutine(Timer2(1f));
+        }
+            
+        if (MaxSpawn == 15&& _Spawn2)
+        {
+            MaxSpawn = 0;
+            bearcount = 0;
+            wolfcount = 0;
+            deercount = 0;
+            raccooncount = 0;
+            foxcount = 0;
+            tigercount = 0;
+            MaxSpawn = 0;
+        }
+        // Q&A 2
+    }
+
+
+
+    IEnumerator Timer1(float t)
+    {
+        _Spawn1 = false;
         int min = 0;
-        int max = 10;
+        int max = 3;
         int x;
         x = Random.Range(min, max);
-        if (x == 1 && MaxSpawn<10) {
+        if (x == 1 )
+        {
             Instantiate(bear, new Vector3(20, 0, Random.Range(45f, 48f)), Quaternion.Euler(0, 90, 0));
             bearcount++;
             MaxSpawn++;
         }
-        if (x == 2 && MaxSpawn < 10)
+        if (x == 2 )
         {
             Instantiate(wolf, new Vector3(20, 0, Random.Range(45f, 48f)), Quaternion.Euler(0, 90, 0));
             wolfcount++;
             MaxSpawn++;
         }
-        if (x == 3 && MaxSpawn < 10)
+        if (x == 3 )
         {
             Instantiate(deer, new Vector3(20, 0, Random.Range(45f, 48f)), Quaternion.Euler(0, 90, 0));
             deercount++;
             MaxSpawn++;
         }
+        yield return new WaitForSeconds(t);
+        _Spawn1 = true;
     }
-
-
-    void Spawn2()
+    IEnumerator Timer2(float t)
     {
-        // Stage 2
+        _Spawn2 = false;
         int min = 0;
         int max = 10;
         int x;
@@ -85,24 +139,13 @@ public class AnimalSpawner : MonoBehaviour
         }
         if (x == 6 && MaxSpawn < 15)
         {
+            _Spawn2 = false;
             Instantiate(tiger, new Vector3(20, 0, Random.Range(45f, 48f)), Quaternion.Euler(0, 90, 0));
             tigercount++;
             MaxSpawn++;
+            yield return new WaitForSeconds(t);
+            _Spawn2 = true;
         }
-    }
-
-
-
-    private void Update()
-    {
-        StartCoroutine(SpawnTime());
-
-    }
-
-    IEnumerator SpawnTime() {
-
-        yield return new WaitForSeconds(2);
-        Spawn1();
 
     }
 }
