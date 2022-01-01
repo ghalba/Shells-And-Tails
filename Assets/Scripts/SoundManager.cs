65c4ae1391bc8dbeaef7ad3a3249867e5973c7d3
saifxss <60525693+saifxss@users.noreply.github.com>
2022-01-01 16:41:34 +0100

    test

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
-    Slider MusicSlider;
+    public Slider MusicSlider;
     // Start is called before the first frame update
     void Start()
     {
