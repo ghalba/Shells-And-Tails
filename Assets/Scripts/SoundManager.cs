dcdc380b7d33533bf20eff91188f3b958d6da750
ghalba <cherifaghalba@gmail.com>
2022-01-01 13:58:21 +0100

    sound

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    Slider MusicSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void MusicVolume()
+    {
+        AudioListener.volume = MusicSlider.value;
+    }
+}
